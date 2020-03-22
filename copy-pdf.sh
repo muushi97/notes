afbfcca49b1a03a8b513a06a6517b28858b6fc90
kohei nishimuta <moouushi@gmail.com>
2020-03-22 22:49:28 +0900

    add some links of the pdf file to index

---
+++ copy-pdf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp algebra/algebra.pdf                                      docs/
+cp bookkeeping/bookkeeping.pdf                              docs/
+cp chinese-remainder-theorem/chinese-remainder-theorem.pdf  docs/
+cp perelman-sequence/perelman-sequence.pdf                  docs/
+cp reed-solomon-coding/reed-solomon-coding.pdf              docs/
+cp russell-s-paradox/russell-s-paradox.pdf                  docs/
+
